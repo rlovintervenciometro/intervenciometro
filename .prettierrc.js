36b668fb2ebe0d069938450db98bf38fe91ac5a8
rlovintervenciometro <ricardointervenciometro@gmail.com>
2022-08-05 23:53:33 -0500

    Configure eslint and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+//prettier
+module.exports = {
+    bracketSpacing: true,
+    arraySpacing: true,
+    jsxBracketSameLine: true,
+    singleQuote: false,
+    trailingComma: 'all',
+    arrowParens: 'avoid'
+}
